a04259aefb4f55497a4ea7c6c8d37615dd6de49d
Andres Gonzalez <andres.gonzalez-ca@mail.escuelaing.edu.co>
2020-04-08 15:15:03 -0500

    Despliegue heroku

--- src/main/java/edu/eci/cvds/samples/manegedbeans/AutenticacionBean.java
+++ src/main/java/edu/eci/cvds/samples/manegedbeans/AutenticacionBean.java
@@ -38,6 +38,8 @@ public class AutenticacionBean {
         }
         if(usu==null){
             estado="Correo o Clave incorrecta";
+        }else{
+            estado="Autenticado";
         }
         
     }
