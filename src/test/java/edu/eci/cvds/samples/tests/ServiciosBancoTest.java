32816bcec09971a09b52780489ceb9b3b0545cc6
Andres Gonzalez <andres.gonzalez-ca@mail.escuelaing.edu.co>
2020-04-24 22:33:55 -0500

    Corrección errores

--- src/test/java/edu/eci/cvds/samples/tests/ServiciosBancoTest.java
+++ src/test/java/edu/eci/cvds/samples/tests/ServiciosBancoTest.java
@@ -6,6 +6,7 @@
 package edu.eci.cvds.samples.tests;
 
 import com.google.inject.Inject;
+import edu.eci.cvds.samples.entities.Area;
 import edu.eci.cvds.samples.entities.Iniciativa;
 import edu.eci.cvds.samples.entities.Rol;
 import edu.eci.cvds.samples.entities.Usuario;
@@ -247,7 +248,7 @@ public class ServiciosBancoTest {
     }
 
 
-    */
+    
      @Test
      public void ConsultarIniciativasPorArea(){
          try{
